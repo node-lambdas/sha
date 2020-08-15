e08a284c35f8579901017e051e1f746d56a78aa7
Darlan Alves <me@darlanalv.es>
2020-08-16 01:11:35 +0200

    feat: implement SHA hashing

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
-import { lambda } from 'node-lambdas';
+import { lambda } from "node-lambdas";
+import { createHash } from "crypto";
 
-lambda((input, output) => input.pipe(output));
+lambda({ readBody: true }, (input, output) => {
+  const type = Number(input.url.slice(1));
+
+  switch (type) {
+    case 1:
+    case 256:
+    case 512:
+      const hash = createHash("sha" + type);
+      output.send(hash.update(input.body).digest("hex"));
+      break;
+
+    default:
+      output.reject("Invalid hash type. Must be one of 1, 256 or 512");
+      return;
+  }
+});
