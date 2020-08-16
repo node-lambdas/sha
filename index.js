496c08a456b356d3c4d1852519cee6a965db8359
Darlan Alves <me@darlanalv.es>
2020-08-17 00:35:46 +0200

    fix: update syntax to use core module

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
-import { lambda } from "node-lambdas";
-import { createHash } from "crypto";
+import { lambda } from '@node-lambdas/core';
+import { createHash } from 'crypto';
 
-lambda({ readBody: true }, (input, output) => {
+const configuration = {
+  version: 1,
+  readBody: true,
+};
+
+lambda(configuration, (input, output) => {
   const type = Number(input.url.slice(1));
 
   switch (type) {
     case 1:
     case 256:
     case 512:
-      const hash = createHash("sha" + type);
-      output.send(hash.update(input.body).digest("hex"));
+      const hash = createHash('sha' + type);
+      output.send(hash.update(input.body).digest('hex'));
       break;
 
     default:
-      output.reject("Invalid hash type. Must be one of 1, 256 or 512");
+      output.reject('Invalid hash type. Must be one of 1, 256 or 512');
       return;
   }
 });
