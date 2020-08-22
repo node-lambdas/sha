c71ffaf81e0904c0142ec90d9b85e853e61814fd
Darlan Alves <me@darlanalv.es>
2020-08-23 00:40:36 +0200

    feat: update to v2

--- index.js
+++ index.js
@@ -2,23 +2,28 @@ import { lambda, Format } from '@node-lambdas/core';
 import { createHash } from 'crypto';
 
 const configuration = {
-  version: 1,
-  input: Format.Buffer,
+  version: 2,
+  actions: {
+    default: {
+      input: Format.Buffer,
+      handler: (input, output) => {
+        const type = Number(input.options.type);
+
+        switch (type) {
+          case 1:
+          case 256:
+          case 512:
+            const hash = createHash('sha' + type);
+            output.send(hash.update(input.body).digest('hex'));
+            break;
+
+          default:
+            output.reject('Invalid hash type. Must be one of 1, 256 or 512');
+            return;
+        }
+      },
+    },
+  },
 };
 
-lambda(configuration, (input, output) => {
-  const type = Number(input.url.slice(1));
-
-  switch (type) {
-    case 1:
-    case 256:
-    case 512:
-      const hash = createHash('sha' + type);
-      output.send(hash.update(input.body).digest('hex'));
-      break;
-
-    default:
-      output.reject('Invalid hash type. Must be one of 1, 256 or 512');
-      return;
-  }
-});
+lambda(configuration);
